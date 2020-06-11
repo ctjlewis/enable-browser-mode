e24152164bea2728a09c632e6b3abb78553cad28
Christian Lewis <ctjl96@gmail.com>
2020-06-11 17:01:07 -0500

    BULK AUTOMATIC COMMIT

--- index.js
+++ index.js
@@ -3,18 +3,16 @@ const DOMWindow = new JSDOM('<html><body></body></html>', {
     url: "https://localhost"
 }).window;
 
-const window = Object.assign(global, DOMWindow);
-console.log(window);
-
 // shhh! don't tell the JSDOM team!
+const window = Object.assign(global, DOMWindow);
 
 /* TESTING */
+// console.log(window);
 // global.testing = 29;
 // console.log(window.testing);
 // console.log(global.testing);
 // console.log(testing);
 
-// require('./test.js');
 const fs = require('fs');
 function windowRequire(file) {
     let contents = fs.readFileSync(file, 'utf-8');
@@ -23,5 +21,5 @@ function windowRequire(file) {
     }(contents));
     // return (1,eval)(contents);
 }
-windowRequire('./test.js');
+windowRequire('./test.js')
 console.log(window.test1, global.test2, test3, test4);
