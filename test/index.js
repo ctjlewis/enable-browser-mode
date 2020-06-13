b64a3750a7ca9a87a167f706270fda9f81d0eb3c
Christian Lewis <ctjl96@gmail.com>
2020-06-12 19:14:38 -0500

    BULK AUTOMATIC COMMIT

--- test/index.js
+++ test/index.js
@@ -1,17 +1,26 @@
 require('..');
 
-window.include('../test.js');
+window.include('test.js');
 console.log("Trying globals:");
-console.log(window.test1, global.test2, test3, test4);
+let globals = [window.test1, global.test2, test3, test4];
+console.log(...globals);
+console.log(globals.filter(x => x).length, "/", 4, "GLOBALS PASSED")
+console.log('');
 
 console.log('window.addEventListener:', window.addEventListener);
 console.log('document.addEventListener:', document.addEventListener);
+console.log('');
 
 console.log("Trying to load jQuery:");
 window.include('./jquery.min.js');
 console.log("PASSED JQUERY TEST:", $ === window.$ && window.$ === global.$);
-// console.log('Adding event listener', DOMWindow.addEventListener('load', console.log).bind(DOMWindow));
+console.log('');
+
+console.log('Adding event listener...');
+window.addEventListener('load', (e) => console.log("WINDOW.LOAD:", e));
+console.log('');
 
 console.log("Trying to load web-widgets...");
 window.include('./widgets.js');
 console.log(Widget);
+console.log('');
