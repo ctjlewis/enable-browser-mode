dafc546f7c3970db5f3699f21a316674789fd358
Christian Lewis <ctjl96@gmail.com>
2020-07-10 18:04:01 -0500

    BULK AUTOMATIC COMMIT: Fri 10 Jul 2020 06:04:01 PM CDT

--- index.js
+++ index.js
@@ -75,5 +75,5 @@ include = function (file) {
 }
 
 // export `execute` for CLI ops in Node
-const include = require('./bin/execute.js');
-module.exports = include;
+const execute = require('./bin/execute.js');
+module.exports = execute;
